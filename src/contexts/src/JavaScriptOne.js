5e2c1318716d89b0eeaad7a5f614de860832ac00
Nathaniel <nathans.stm@aol.com>
2024-12-16 22:54:34 +0000

    Delete all files

--- src/contexts/src/JavaScriptOne.js
+++
@@ -1,9 +0,0 @@
-export const Json = {
-  "1234567890": {
-    title: 'JSON',
-    language: 'json',
-    code: '{\n  "name": "John",\n  "age": 30\n}'
-  }
-};
-
-
--- src/contexts/src/Json.js
+++
@@ -1,9 +0,0 @@
-export const Json = {
-  "9876543210": {
-    title: 'JSON',
-    language: 'json',
-    code: '{\n  "name": "John",\n  "age": 30\n}'
-  }
-};
-
-
